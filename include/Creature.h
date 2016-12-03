d6fbd2728e92ed85969e417774d497dfc000ad01
redzhepdx <redzhepdx@users.noreply.github.com>
2016-12-03 21:54:55 +0200

    Add files via upload

--- include/Creature.h
+++ include/Creature.h
@@ -3,54 +3,95 @@
 
 #include <Box2D/Box2D.h>
 #include <SFML/Graphics.hpp>
+#include <memory>
 
+class Node;
 class Muscle {
-    public:
-        Muscle(b2World* world, b2DistanceJointDef& def, float short_len_, float long_len_,
-               float extend_time_, float contract_time_, float strength_);
+public:
+	Muscle();
+	virtual ~Muscle();
 
-        void update(float c_time, float dt);
-        void render(sf::RenderTarget& rt);
+	void init(b2World* world_, std::vector<std::unique_ptr<Node>>& nodes, int a_, int b_, float short_len_, float long_len_,
+		float extend_time_, float contract_time_, float strength_);
+	void update(float c_time, float dt);
+	void setActive(bool active);
+	void render(sf::RenderTarget& rt);
+	std::unique_ptr<Muscle> mutatedCopy(std::vector<std::unique_ptr<Node>>& node);
+	std::unique_ptr<Muscle> copy(std::vector<std::unique_ptr<Node>>& node);
 
-        b2DistanceJoint* joint = nullptr;
+	b2DistanceJoint* joint = nullptr;
 
-        float short_len = 1.5f;
-        float long_len = 3.0f;
-        float* target_len = nullptr;
+	float short_len;
+	float long_len;
+	float* target_len = nullptr;
 
-        float extend_time = 0.75f;
-        float contract_time = 0.25f;
+	float extend_time;
+	float contract_time;
 
-        float strength = 1.0f;
+	float strength;
+	sf::Color c;
+	b2World* world = nullptr;
+	int a = -1, b = -1;
 };
 
-class Node{
-    public:
-        Node();
-        virtual ~Node();
+class Node {
+public:
+	Node();
+	virtual ~Node();
 
-        void init(b2World* world, const sf::Vector2f& pos, float friction);
-        void setPosition(const sf::Vector2f& pos);
-        void render(sf::RenderTarget& rt);
-        sf::Color c;
-        b2CircleShape shape;
-        b2FixtureDef fixture_def;
-        b2BodyDef body_def;
-        b2Body* body = nullptr;
+	void init(b2World* world_, const sf::Vector2f& pos, float friction);
+	void setActive(bool active);
+	sf::Vector2f getPosition() const;
+	void setPosition(const sf::Vector2f& pos);
+	void render(sf::RenderTarget& rt);
+	std::unique_ptr<Node> mutatedCopy();
+	std::unique_ptr<Node> copy();
+
+	sf::Color c;
+	b2CircleShape shape;
+	b2FixtureDef fixture_def;
+	b2BodyDef body_def;
+	b2Body* body = nullptr;
+	b2World* world = nullptr;
 };
 
 class Creature {
-    public:
-        Creature();
+public:
+	Creature();
 
-        void init(b2World* world, const sf::Vector2f& pos);
-        void update(float dt);
-        void render(sf::RenderTarget& rt);
-        std::vector<Node> nodes;
-        std::vector<Muscle> muscles;
+	void init(b2World* world_, const sf::Vector2f& position);
+	void updatePosition();
+	void setPosition(const sf::Vector2f& position);
+	void update(float dt, bool update_fitness = true);
+	void render(sf::RenderTarget& rt);
+	const sf::Vector2f& getPosition() const { return pos; };
 
-        float timer = 0.0f;
-        float heart_beat = 0.5f;
+	void saveSnapshot(std::unique_ptr<Creature>& out);
+	void reset(std::unique_ptr<Creature>& ref_to_me);
+
+	void addRandomNode();
+	void addNode(std::unique_ptr<Node> node);
+	void addMuscle(int a = -1, int b = -1);
+	void setActive(bool active);
+	std::unique_ptr<Creature> mutatedCopy();
+	std::unique_ptr<Creature> copy();
+	std::unique_ptr<Creature> normalCopy();
+	void removeRandomNode();
+	void removeRandomMuscle();
+	void checkMuscleOverlap();
+	void checkLoneNodes();
+
+	std::vector<std::unique_ptr<Node>> nodes;
+	std::vector<std::unique_ptr<Muscle>> muscles;
+
+	float timer = 0.0f;
+	float heart_beat = 0.5f;
+	float fitness = 0.0f;
+
+	b2World* world = nullptr;
+	std::unique_ptr<Creature> snapshot;
+private:
+	sf::Vector2f pos;
 };
 
 #endif // CREATURE_H
--- include/Game.h
+++ include/Game.h
@@ -8,43 +8,59 @@
 #include "Creature.h"
 
 class Game {
-        static Game m_instance;
-    public:
-        static Game& i() { return m_instance; }
+	static Game m_instance;
+public:
+	static Game& i() { return m_instance; }
 
-        void init(float _dt);
-        void run();
-        void restart();
-        void update();
-        void render();
+	void init(float _dt);
+	void run();
+	void restart();
+	void update();
+	void render();
 
-        float getDeltaTime();
-        void setDeltaTime(float _dt);
-        float getTime();
-        sf::Vector2f getViewSize();
+	float getDeltaTime();
+	void setDeltaTime(float _dt);
+	float getTime();
+	sf::Vector2f getViewSize();
 
+	void drawRect(sf::VertexArray& va, b2Body* b, b2Fixture* f, const sf::Vector2f& dim, const sf::Color& c = sf::Color::Magenta);
+	void spawnCreature();
+	std::unique_ptr<Creature> crossoveredCreature(std::unique_ptr<Creature> &parent_1, std::unique_ptr<Creature> &parent_2);
+	sf::Vector2f getSpawnPos();
+private:
+	float m_accumulator = 0.0f;
+	float m_dt = 1 / 125.0f;
+	sf::RenderWindow m_window;
+	sf::Vector2f m_view_size;
+	float m_global_timer = 0.0f;
+	bool m_first_launch = true;
+	sf::Font m_font;
 
-        void drawRect(sf::VertexArray& va, b2Body* b, b2Fixture* f, const sf::Vector2f& dim, const sf::Color& c = sf::Color::Magenta);
-        void spawnBox();
-    private:
-        float m_accumulator = 0.0f;
-        float m_dt = 1 / 125.0f;
-        sf::RenderWindow m_window;
-        sf::Vector2f m_view_size;
-        float m_global_timer = 0.0f;
-        bool m_first_launch = true;
-        sf::Font m_font;
+	sf::View view;
+	b2Vec2 gravity;
+	std::unique_ptr<b2World> m_world;
+	std::vector<std::unique_ptr<Creature>> m_creatures;
 
-        sf::View view;
-        b2Vec2 gravity;
-        std::unique_ptr<b2World> m_world;
-        std::vector<Creature> m_creatures;
+	b2Body* groundBody;
+	b2Fixture* groundFixture;
+	sf::Vector2f groundDim;
 
-        b2Body* groundBody;
-        b2Fixture* groundFixture;
-        sf::Vector2f groundDim;
+	sf::Vector2f viewDimensions;
 
-        sf::Vector2f viewDimensions;
+	float best_x = 0.0f;
+	// Genetic Algorithm
+	unsigned gen = 1;
+	unsigned curr_creature_id = 0;
+
+	bool j_last_frame = false;
+	bool simulate_quick = false;
+
+	bool cam_key_last_frame = false;
+	bool show_best = false;
+
+	std::unique_ptr<Creature> best_creature = nullptr;
+	int best_c_gen = 0;
+	int best_c_id = 0;
 };
 
 #endif // GAME_H
